e190e30e7f768c2301a884e38c3dc3fd7cd7128c
Semanyoh Nissi <semanyohn@gmail.com>
2022-09-10 20:16:29 -0700

    value changed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			for (t = tens; t <= '9'; t++)
 			{
-				for (0 = ones + 1; 0 <= '9'; o++)
+				for (o = ones + 1; 0 <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
