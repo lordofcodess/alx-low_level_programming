1fdd660163ab5c3e8ffe85956d650bd8e6299f0a
Semanyoh Nissi <semanyohn@gmail.com>
2022-09-12 07:18:18 -0700

    retyping code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,14 +18,14 @@ int main(void)
 	{
 		printf("%i is positive\n", n);
 	}
-	else if (n == 0)
+	else if (n == o)
 	{
 		printf("%i is zero\n", n);
-        }
-        else if (n < 0)
-        {
+	}
+	else if (n < 0)
+	{
 		printf("%i is negative\n", n);
-        }
+	}
 
-        return (0);
+	return (0);
 }
